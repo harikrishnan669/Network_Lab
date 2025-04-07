93416f7c37d8af14828a424ed13f44f338d26b5c
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2025-04-07 16:04:58 +0530

    Update client.c

--- Matrix Multiplication(UDP)/client.c
+++ Matrix Multiplication(UDP)/client.c
@@ -8,51 +8,45 @@
 #define SERVER_IP "127.0.0.1"
 #define size 2
 
-int main()
-{
-int sockfd;
-struct sockaddr_in server_addr;
-socklen_t addr_len=sizeof(server_addr);
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    socklen_t addr_len = sizeof(server_addr);
+    printf("Socket created Successfully...\n");
 
-sockfd=socket(AF_INET,SOCK_DGRAM,0);
-server_addr.sin_family=AF_INET;
-server_addr.sin_port=htons(PORT);
-server_addr.sin_addr.s_addr=inet_addr(SERVER_IP);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-int A[size][size],B[size][size],result[size][size];
+    int A[size][size], B[size][size], result[size][size];
 
-printf("Enter the elements of matrix A:\n");
-for(int i=0;i<size;i++)
-{
-  for(int j=0;j<size;j++)
-  {
-    scanf("%d",&A[i][j]);
-  }
-}
+    printf("Enter the elements of matrix A:\n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
 
-printf("Enter the elements of matrix B:\n");
-for(int i=0;i<size;i++)
-{
-  for(int j=0;j<size;j++)
-  {
-    scanf("%d",&B[i][j]);
-  }
-}
+    printf("Enter the elements of matrix B:\n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
 
-sendto(sockfd,A,sizeof(A),0,(struct sockaddr*)&server_addr,addr_len);
-sendto(sockfd,B,sizeof(B),0,(struct sockaddr*)&server_addr,addr_len);
+    sendto(sockfd, A, sizeof(A), 0, (struct sockaddr *) &server_addr, addr_len);
+    sendto(sockfd, B, sizeof(B), 0, (struct sockaddr *) &server_addr, addr_len);
 
-recvfrom(sockfd,result,sizeof(result),0,(struct sockaddr*)&server_addr,&addr_len);
+    recvfrom(sockfd, result, sizeof(result), 0, (struct sockaddr *) &server_addr, &addr_len);
 
-printf("The resultant matrix is:\n");
-for(int i=0;i<size;i++)
-{
- for(int j=0;j<size;j++)
- {
-   printf("%d",result[i][j]);
- }
-  printf("\n");
-}
-close(sockfd);
-return 0;
+    printf("The resultant matrix is: \n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+    close(sockfd);
+    return 0;
 }
